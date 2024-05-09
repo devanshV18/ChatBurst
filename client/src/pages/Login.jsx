64249206e621113ce4b6845215778575120bbfba
devanshV18 <devanshverma024@gmail.com>
2024-05-09 16:58:40 +0530

    register ui done

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -5,7 +5,7 @@ const Login = () => {
 
     const [isLogin, setIsLogin] = useState(true)
 
-    const toggleLogin = () => setIsLogin(false)
+    const toggleLogin = () => setIsLogin((prev => !prev))
 
   return (
     <Container component={"main"} maxWidth="xs" sx={{
@@ -34,7 +34,19 @@ const Login = () => {
                     </form>
                 </>
                 : 
-                <span>Register</span>}
+                <>
+                    <Typography variant='h5'>Sign Up</Typography>
+                    <form>
+                        <TextField required fullWidth label="Name" margin="normal" variant="outlined"/>
+                        <TextField required fullWidth label="Bio" margin="normal" variant="outlined"/>
+                        <TextField required fullWidth label="username" margin="normal" variant="outlined"/>
+                        <TextField required fullWidth label="password" type='password' margin="normal" variant="outlined"/>
+                        <Button sx = {{marginTop: "1rem"}} variant='contained' color='primary' type='submit' fullWidth>Sign Up</Button>
+                        <Typography textAlign={"center"} m={"1rem"}>OR</Typography>
+                        <Button fullWidth variant='text' onClick={toggleLogin}>Log In Instead</Button>
+                    </form>
+                </>
+                }
         </Paper>
     </Container>
   )
