b21ebb2fbdf1d104f2aebfe52522fbef216aa803
devanshV18 <devanshverma024@gmail.com>
2024-05-09 19:10:36 +0530

    logina nd register page ui doen with validations

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,5 +1,9 @@
 import React, { useState } from 'react'
-import {Paper, Container, Typography, TextField, Button} from "@mui/material"
+import {Paper, Container, Typography, TextField, Button, Stack, Avatar, IconButton} from "@mui/material"
+import {CameraAlt as CameraAltIcon} from "@mui/icons-material"
+import { VisuallyHiddenInput } from '../components/styles/StyledComponents'
+import {useFileHandler, useInputValidation} from "6pp"
+import { usernameValidator } from '../utils/validators'
 
 const Login = () => {
 
@@ -7,8 +11,28 @@ const Login = () => {
 
     const toggleLogin = () => setIsLogin((prev => !prev))
 
+    const name = useInputValidation("")
+    const username = useInputValidation("",usernameValidator)
+    const bio = useInputValidation("")
+    const password = useInputValidation("")
+
+    const avatar = useFileHandler("single")
+
+    const handleLogin = (e) => {
+        e.preventDefault()
+    }
+
+    const handleSignUp = (e) => {
+        e.preventDefault()
+    }
+
   return (
-    <Container component={"main"} maxWidth="xs" sx={{
+    
+    <div style={{backgroundImage:"linear-gradient(rgba(255,255,209),rgba(249,159,159))"}}>
+
+        
+
+        <Container component={"main"} maxWidth="xs" sx={{
         height: "100vh",
         display: "flex",
         justifyContent: "center",
@@ -25,9 +49,12 @@ const Login = () => {
                 {isLogin ? 
                 <>
                     <Typography variant='h5'>Login</Typography>
-                    <form>
-                        <TextField required fullWidth label="username" margin="normal" variant="outlined"/>
-                        <TextField required fullWidth label="password" type='password' margin="normal" variant="outlined"/>
+                    <form
+                        style={{width: "100%", marginTop: "1rem" }}
+                        onSubmit={handleLogin}
+                    >
+                        <TextField required fullWidth label="username" margin="normal" variant="outlined" value={username.value} onChange={username.changeHandler}/>
+                        <TextField required fullWidth label="password" type='password' margin="normal" variant="outlined" value={password.value} onChange={password.changeHandler}/>
                         <Button sx = {{marginTop: "1rem"}} variant='contained' color='primary' type='submit' fullWidth>Login</Button>
                         <Typography textAlign={"center"} m={"1rem"}>OR</Typography>
                         <Button fullWidth variant='text' onClick={toggleLogin}>Sign Up Instead</Button>
@@ -36,11 +63,44 @@ const Login = () => {
                 : 
                 <>
                     <Typography variant='h5'>Sign Up</Typography>
-                    <form>
-                        <TextField required fullWidth label="Name" margin="normal" variant="outlined"/>
-                        <TextField required fullWidth label="Bio" margin="normal" variant="outlined"/>
-                        <TextField required fullWidth label="username" margin="normal" variant="outlined"/>
-                        <TextField required fullWidth label="password" type='password' margin="normal" variant="outlined"/>
+                    <form
+                    style={{width: "100%", marginTop: "1rem" }}
+                    onSubmit={handleSignUp}
+                    >
+                        <Stack position={"relative"} width={"10rem"} margin={"auto"}>
+                            <Avatar sx={{
+                                width: "10rem",
+                                height: "10rem",
+                                objectFit: "contain"
+                            }}
+                            src={avatar.preview}
+                            />
+
+                            {avatar.error && (
+                            <Typography m={"1rem auto"} width={"fit-content"} display={"block"} color="error" variant="caption">{avatar.error}</Typography>
+                            )}  
+
+                            <IconButton
+                            sx={{position: "absolute", bottom: "0", right: "0", color:"white", bgcolor: "rgba(0,0,0,0.5)",":hover":{
+                                bgcolor: "rgba(0,0,0,0.7)",
+                            },
+                            }}
+                            component = "label"
+                            >
+                                <>
+                                    <CameraAltIcon/>
+                                    <VisuallyHiddenInput type='file' onChange={avatar.changeHandler}/>
+                                </>
+                            </IconButton>
+                        </Stack>
+                        <TextField required fullWidth label="Name" margin="normal" variant="outlined" value={name.value} onChange={name.changeHandler}/>
+                        <TextField required fullWidth label="Bio" margin="normal" variant="outlined" value={bio.value} onChange={bio.changeHandler}/>
+                        <TextField required fullWidth label="username" margin="normal" variant="outlined" value={username.value} onChange={username.changeHandler}/>
+                        {username.error && (
+                            <Typography color="error" variant="caption">{username.error}</Typography>
+                        )}
+                        <TextField required fullWidth label="password" type='password' margin="normal" variant="outlined" value={password.value} onChange={password.changeHandler}/>
+                       
                         <Button sx = {{marginTop: "1rem"}} variant='contained' color='primary' type='submit' fullWidth>Sign Up</Button>
                         <Typography textAlign={"center"} m={"1rem"}>OR</Typography>
                         <Button fullWidth variant='text' onClick={toggleLogin}>Log In Instead</Button>
@@ -49,6 +109,7 @@ const Login = () => {
                 }
         </Paper>
     </Container>
+    </div>
   )
 }
 
