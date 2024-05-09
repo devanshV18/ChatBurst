848cbf32b8dcd451da2c23ffc024bff694f5e8d6
devanshV18 <devanshverma024@gmail.com>
2024-05-09 16:49:09 +0530

    basic login ui complete

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,10 +1,37 @@
-import React from 'react'
+import React, { useState } from 'react'
+import {Paper, Container, Typography, TextField, Button} from "@mui/material"
 
 const Login = () => {
+
+    const [isLogin, setIsLogin] = useState(true)
+
+    const toggleLogin = () => setIsLogin(false)
+
   return (
-    <div>
-      Login
-    </div>
+    <Container>
+        <Paper
+            elevation={3}
+            sx={{
+                padding: 4,
+                display: "flex",
+                flexDirection: 'column',
+                alignItems: 'center'
+            }}>
+                {isLogin ? 
+                <>
+                    <Typography variant='h5'>Login</Typography>
+                    <form>
+                        <TextField required fullWidth label="username" margin="normal" variant="outlined"/>
+                        <TextField required fullWidth label="password" type='password' margin="normal" variant="outlined"/>
+                        <Button sx = {{marginTop: "1rem"}} variant='contained' color='primary' type='submit' fullWidth>Login</Button>
+                        <Typography textAlign={"center"} m={"1rem"}>OR</Typography>
+                        <Button sx = {{marginTop: "1rem"}} fullWidth variant='text' onClick={toggleLogin}>Sign Up Instead</Button>
+                    </form>
+                </>
+                : 
+                <span>Register</span>}
+        </Paper>
+    </Container>
   )
 }
 
