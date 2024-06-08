2404fae5e3fd335e97665a6a5c00565dee01adc8
devanshV18 <devanshverma024@gmail.com>
2024-06-08 22:25:21 +0530

    render attachment debugged

--- client/src/components/shared/RenderAttachment.jsx
+++ client/src/components/shared/RenderAttachment.jsx
@@ -1,7 +1,32 @@
 import React from 'react'
+import { transformImage } from '../../lib/features';
+import { FileOpen as FileOpenIcon } from '@mui/icons-material';
 
 const RenderAttachment = ({file,url}) => {
-  
+  switch (file) {
+    case "video": 
+        <video src={url} preload='none' width={"200px"} controls></video>
+        break;
+
+    case "image":
+        <img src={transformImage(url,200)} 
+        alt="attachment" 
+        width={"200px"}
+        height={"150px"}
+        style = {{
+            objectFit: "contain"
+        }}
+        />
+        break;
+
+    case "audio":
+        <audio src={url} preload='none' controls></audio>
+        break;
+
+    default:
+        <FileOpenIcon/>
+        break;
+  }
 }
 
 export default RenderAttachment
