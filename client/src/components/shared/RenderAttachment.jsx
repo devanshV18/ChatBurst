07651b21dbdf440fbc041b8cab92f7703ef04234
devanshV18 <devanshverma024@gmail.com>
2024-06-08 22:21:33 +0530

    retrying RenderAttachment

--- client/src/components/shared/RenderAttachment.jsx
+++ client/src/components/shared/RenderAttachment.jsx
@@ -1,28 +1,8 @@
-import { BreakfastDiningOutlined } from '@mui/icons-material'
 import React from 'react'
-import { transformImage } from '../../lib/features';
 
 const RenderAttachment = ({file,url}) => {
-  switch(file){
-    case "video":
-        <video src="url" preload='none' width={"200px"} controls></video>
-        break;
-
-    case "image":
-        <img 
-        src={transformImage(url)} 
-        alt="attachment Image" 
-        width={"200px"} 
-        height={"150px"} 
-        style={{
-            objectFit: "contain"
-        }} 
-        />
-        break
-
-    default:
-        break;
-  }
+  
 }
 
 export default RenderAttachment
+
